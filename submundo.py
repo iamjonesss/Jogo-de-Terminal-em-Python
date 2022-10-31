dfc7ee5cd5fa2e0bafcc9d7739919368084aaee4
João Souza <91389107+iamjonesss@users.noreply.github.com>
2022-10-31 15:04:42 -0300

    Update submundo.py

--- submundo.py
+++ submundo.py
@@ -1,5 +1,9 @@
                                                     #ESTUDO DE POO
+
+#IMPORTAÇÕES
 import os
+import random
+from time import sleep
 
 #Criação da classe
 class Player():
@@ -11,48 +15,100 @@ class Player():
         self.inteligencia = 0
         self.sabedoria = 0
         self.carisma = 0
-        self.vida = 32
+        self.vida = 0
         self.armas = ''
+        self.skills = ''
         
 player1 = Player()
 player2 = Player()
+iniciativaPlayer1 = random.randrange(1, 20)
+iniciativaPlayer2 = random.randrange(1, 20)
+
+
+
 
 def PersonagensPlayer1():
     #Limpar o terminal
     os.system('cls')
-    print('========== SELEÇÃO DO PERSONAGEM ==========')
+    print('========== SELEÇÃO DO PERSONAGEM PLAYER 1 ==========')
     
     #Laço de seleção de personagem
     while True:
-        #Onde será feito a escolha
-        escolha = input("Escolha um personagem:\n 1- Kenji\n 2- Aoki\n 3- Hei\n 4- Hideki\n 5- Edward\n \n Número: ")
+        #Onde será feito a escolha1
+        escolha1 = input("Player 1: Escolha um personagem:\n 1- Kenji\n 2- Aoki\n 3- Hei\n 4- Hideki\n 5- Edward\n \n Número: ")
         
         #Processamento das escolhas
-        if (escolha == "1"):
+        if (escolha1 == "1"):
             player1.forca = 1
             player1.destreza = 5
             player1.constituicao = 2
             player1.sabedoria = 0
             player1.carisma = 1
             player1.vida = 32
-            player1.armas = 'Duas Wakizashis'
+            player1.armas = ['Duas Wakizashis', 'Arco Longo', 'Corpo a Corpo']
+            player1.skills = ['Golpe Esquentado', 'Ataque com Arco Longo', 'Ataque duplo de Wakizashi', 'Cura com frasco de sangue']
             break
     
-        elif (escolha == "2"):
+        elif (escolha1 == "2"):
             pass
             break
-        elif(escolha == "3"):
+        elif(escolha1 == "3"):
             pass
             break
-        elif(escolha == "4"):
+        elif(escolha1 == "4"):
             pass
             break
-        elif (escolha == "5"):
+        elif (escolha1 == "5"):
             pass
             break
         else:
             print("\nNúmero inválido, digite a enumeração correta do personagem desejado\n")
             
+
+def PersonagensPlayer2():
+    #Limpar o terminal
+    os.system('cls')
+    print('========== SELEÇÃO DO PERSONAGEM PLAYER 2 ==========')
+    
+    #Laço de seleção de personagem
+    while True:
+        #Onde será feito a escolha1
+        escolha2 = input("Player 1: Escolha um personagem:\n 1- Kenji\n 2- Aoki\n 3- Hei\n 4- Hideki\n 5- Edward\n \n Número: ")
+        #Processamento das escolhas
+        if (escolha2 == "1"):
+            player2.forca = 1
+            player2.destreza = 5
+            player2.constituicao = 2
+            player2.sabedoria = 0
+            player2.carisma = 1
+            player2.vida = 32
+            player2.armas = ['Duas Wakizashis', 'Arco Longo', 'Corpo a Corpo']
+            player2.skills = ['Golpe Esquentado', 'Ataque com Arco Longo', 'Ataque duplo de Wakizashi', 'Cura com frasco de sangue']
+            break
+    
+        elif (escolha2 == "2"):
+            pass
+            break
+
+        elif(escolha2 == "3"):
+            pass
+            break
+
+        elif(escolha2 == "4"):
+            pass
+            break
+
+        elif (escolha2 == "5"):
+            pass
+            break
+
+        else:
+            print("\nNúmero inválido, digite a enumeração correta do personagem desejado\n")
+
+
+
 #Chamando função de PersonagensPlayer1
 PersonagensPlayer1()
-print(player1.destreza)
+PersonagensPlayer2()
+
+print(f'Girando a Iniciativa do Player 1: {iniciativaPlayer1}\nGirando Iniciativa do Player 2: {iniciativaPlayer2}')
